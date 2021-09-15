8ceb817eaf7dd2400cafde96538dd5e674b4621e
Shivani <Shivani@DESKTOP-ITMO33O>
2021-09-15 12:13:30 +0530

    b

--- LTIAirlines/src/main/java/com/lti/model/Booking.java
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -1,5 +1,20 @@
 package com.lti.model;
 
 public class Booking {
+	int bookingno;
+	Flight flight;
+	public int getBookingno() {
+		return bookingno;
+	}
+	public void setBookingno(int bookingno) {
+		this.bookingno = bookingno;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	
 
 }
