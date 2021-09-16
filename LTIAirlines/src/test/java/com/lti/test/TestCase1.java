a3c4f4acb5bdbd9fcfc873d87b90ef7e7aeea012
shivani2271999 <shivani17151@cse.ssn.edu.in>
2021-09-16 13:11:43 +0530

    neww

--- LTIAirlines/src/test/java/com/lti/test/TestCase1.java
+++ LTIAirlines/src/test/java/com/lti/test/TestCase1.java
@@ -1,37 +1,37 @@
-package com.lti.test;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import com.lti.client.Main;
-import com.lti.model.Flight;
-
-public class TestCase1 {
-
-	@Test
-	public void testAdd() {
-		//test data
-		int num1=10;int num2=10;
-		//assertEquals(70, Main.add(num1, num2));  //checks wheter 2 values are equal
-		assertTrue(Main.compare(2,2));
-	}
-	
-	
+//package com.lti.test;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//import com.lti.client.Main;
+//import com.lti.model.Flight;
+//
+//public class TestCase1 {
+//
 //	@Test
-//	public void test2() {
+//	public void testAdd() {
+//		//test data
+//		int num1=10;int num2=10;
+//		//assertEquals(70, Main.add(num1, num2));  //checks wheter 2 values are equal
 //		assertTrue(Main.compare(2,2));
 //	}
-	
-//	@Test
-//	public void testObjects() {
-//		Flight f1=new Flight(1001,"Mumbai","Delhi");
-//		Flight f2=new Flight(1001,"Mumbai","Delhi");
-//		//Flight f2=f1;
-//		//assertEquals("Objects are",f1, f2); //messages can be passed if error occurs
-//	}
-	
-	
-	
-
-}
+//	
+//	
+////	@Test
+////	public void test2() {
+////		assertTrue(Main.compare(2,2));
+////	}
+//	
+////	@Test
+////	public void testObjects() {
+////		Flight f1=new Flight(1001,"Mumbai","Delhi");
+////		Flight f2=new Flight(1001,"Mumbai","Delhi");
+////		//Flight f2=f1;
+////		//assertEquals("Objects are",f1, f2); //messages can be passed if error occurs
+////	}
+//	
+//	
+//	
+//
+//}
